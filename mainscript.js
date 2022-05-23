9c9c3381b6beec15c945f9093784f237c6e2cd30
Valoeable <samo.valo3@gmail.com>
2022-05-23 20:48:18 +0200

    Clicking on numbers displays a value and stores it

--- mainscript.js
+++ mainscript.js
@@ -1,13 +1,6 @@
-let initVal=0;
+let currVal="0";
 
 function operate(){
-    let a=prompt("Select the first number:",0);
-    a=parseInt(a);
-
-    let b=prompt("Select the second number:",0);
-    b=parseInt(b);
-
-    let op=prompt("Select the operator:");
 
     if(op==="+"){
         return add(a,b);
@@ -44,6 +37,38 @@ function divide(a,b){
     return a/b;
 }
 
-initVal=operate();
 
-console.log(initVal);
+let initVal=document.getElementById("numberDisplay");
+let numOne=document.getElementById("nOne");
+let numTwo=document.getElementById("nTwo");
+let numThree=document.getElementById("nThree");
+let numFour=document.getElementById("nFour");
+let numFive=document.getElementById("nFive");
+let numSix=document.getElementById("nSix");
+let numSeven=document.getElementById("nSeven");
+let numEight=document.getElementById("nEight");
+let numNine=document.getElementById("nNine");
+let numZero=document.getElementById("nZero");
+
+numOne.addEventListener("click",()=>addNumber("1"));
+numTwo.addEventListener("click",()=>addNumber("2"));
+numThree.addEventListener("click",()=>addNumber("3"));
+numFour.addEventListener("click",()=>addNumber("4"));
+numFive.addEventListener("click",()=>addNumber("5"));
+numSix.addEventListener("click",()=>addNumber("6"));
+numSeven.addEventListener("click",()=>addNumber("7"));
+numEight.addEventListener("click",()=>addNumber("8"));
+numNine.addEventListener("click",()=>addNumber("9"));
+numZero.addEventListener("click",()=>addNumber("0"));
+
+
+function addNumber(clickedNum){
+    if(currVal==="0"){
+        currVal=clickedNum;
+        initVal.textContent=`${currVal}`;
+    }
+    else{
+        currVal+=clickedNum;
+        initVal.textContent=`${currVal}`;
+    }
+}
