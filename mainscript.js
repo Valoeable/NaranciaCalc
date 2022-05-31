2488e0a3d5b8f0cd0c17ba554b1780e0478e4c39
Valoeable <samo.valo3@gmail.com>
2022-05-31 13:31:48 +0200

    Enabled clear and operator buttons,enabled calculation in a row

--- mainscript.js
+++ mainscript.js
@@ -1,21 +1,39 @@
-let currVal="0";
+//Helper variables
 
-function operate(){
+let currVal="0",operator="NONE",a=0,b=0,fStart=0;
+
+//Main calculator functions
+
+function operate(op){
+    b=parseInt(currVal);
 
     if(op==="+"){
-        return add(a,b);
+        currVal=add(a,b);
+        initVal.textContent=`${currVal}`;
+        operator="NONE";
+        fStart=1;
     }
 
     if(op==="-"){
-        return sub(a,b);
+        currVal=sub(a,b);
+        initVal.textContent=`${currVal}`;
+        operator="NONE";
+        fStart=1;
     }
 
     if(op==="*"){
-        return mul(a,b);
+        currVal=mul(a,b);
+        initVal.textContent=`${currVal}`;
+        operator="NONE";
+        fStart=1;
     }
 
     if(op==="/"){
-        return divide(a,b);
+        currVal=divide(a,b);
+        currVal=Math.round(currVal*100)/100;
+        initVal.textContent=`${currVal}`;
+        operator="NONE";
+        fStart=1;
     }
 
 }   
@@ -37,6 +55,7 @@ function divide(a,b){
     return a/b;
 }
 
+//Initialization of buttons
 
 let initVal=document.getElementById("numberDisplay");
 let numOne=document.getElementById("nOne");
@@ -49,6 +68,12 @@ let numSeven=document.getElementById("nSeven");
 let numEight=document.getElementById("nEight");
 let numNine=document.getElementById("nNine");
 let numZero=document.getElementById("nZero");
+let pOp=document.getElementById("plus");
+let mOp=document.getElementById("minus");
+let mulOp=document.getElementById("mul");
+let divOp=document.getElementById("dvd");
+let equal=document.getElementById("eq");
+let clear=document.getElementById("clr");
 
 numOne.addEventListener("click",()=>addNumber("1"));
 numTwo.addEventListener("click",()=>addNumber("2"));
@@ -60,15 +85,87 @@ numSeven.addEventListener("click",()=>addNumber("7"));
 numEight.addEventListener("click",()=>addNumber("8"));
 numNine.addEventListener("click",()=>addNumber("9"));
 numZero.addEventListener("click",()=>addNumber("0"));
+pOp.addEventListener("click",()=>opChoice("+"));
+mOp.addEventListener("click",()=>opChoice("-"));
+mulOp.addEventListener("click",()=>opChoice("*"));
+divOp.addEventListener("click",()=>opChoice("/"));
+eq.addEventListener("click",()=>operate(operator));
+clear.addEventListener("click",()=>clearAll());
 
+pOp.disabled=true;
+mOp.disabled=true;
+mulOp.disabled=true;
+divOp.disabled=true;
+
+
+//Selection section
 
 function addNumber(clickedNum){
-    if(currVal==="0"){
-        currVal=clickedNum;
-        initVal.textContent=`${currVal}`;
+
+    if(fStart===1&&operator==="NONE"){
+        currVal="0";
+        fStart=0;
     }
-    else{
+
+    if(currVal!=0){
         currVal+=clickedNum;
         initVal.textContent=`${currVal}`;
     }
+
+    if(currVal==="0"){
+        pOp.style.backgroundColor="black";
+        mOp.style.backgroundColor="black";
+        mulOp.style.backgroundColor="black";
+        divOp.style.backgroundColor="black";
+        currVal=clickedNum;
+        initVal.textContent=`${currVal}`;
+        pOp.disabled=false;
+        mOp.disabled=false;
+        mulOp.disabled=false;
+        divOp.disabled=false;
+
+    }
+    
+}
+
+function opChoice(opChosen){
+    if(opChosen==="+" && operator==="NONE"){
+        pOp.style.backgroundColor="rgb(247, 63, 63)";
+        operator="+";
+        a=parseInt(currVal);
+        currVal="0"
+        return;
+    }
+    
+
+    if(opChosen==="-" && operator==="NONE"){
+        mOp.style.backgroundColor="rgb(247, 63, 63)";
+        operator="-";
+        a=parseInt(currVal);
+        currVal="0"
+        return;
+    }
+   
+
+    if(opChosen==="*" && operator==="NONE"){
+        mulOp.style.backgroundColor="rgb(247, 63, 63)";
+        operator="*";
+        a=parseInt(currVal);
+        currVal="0"
+        return;
+    }
+   
+    if(opChosen==="/" && operator==="NONE"){
+        divOp.style.backgroundColor="rgb(247, 63, 63)";
+        operator="/";
+        a=parseInt(currVal);
+        currVal="0"
+        return;
+    }
+   
+}
+
+function clearAll(){
+    currVal="0",operator="NONE",a=0,b=0,fStart=0;
+    initVal.textContent=`${currVal}`;
 }
